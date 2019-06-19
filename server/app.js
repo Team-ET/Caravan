7212fd3f3a47a4c00994efa473f17a1cd070236d
Trevor Scott <Trvr.Scott1@gmail.com>
2019-06-18 19:27:19 -0500

    'Server and db up and running'

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const db = require('../database/index.js');
+const express = require('express');
+
+const path = require('path');
+
+// const bodyparser = require('body-parser');
+
+const app = express();
+
+// app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../src')));
+
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
