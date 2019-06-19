f1d888d74d70bb040ca0f6916aac44fd9deb0efb
Trevor Scott <Trvr.Scott1@gmail.com>
2019-06-19 15:20:35 -0500

    'Adding auth0'

---
+++ src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+         clientID: "jZslOYmLdnleoatyDPab2dQXuiWs2mX4",
+         domain: "dev-xhza98ci.auth0.com",
+         callbackURL: "http://localhost:4200/callback"
+       };
