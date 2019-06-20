ba314d41f510bb7794cdb9de97ffdec04ff989ce
Trevor Scott <Trvr.Scott1@gmail.com>
2019-06-20 12:16:26 -0500

    'Got env variables working'

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
 
 const personalityInsights = new PersonalityInsightsV3({
-  iam_apikey: 'nk272cV8EpYelBmO-Wd_qe50wmCTYqq7ti00gT2sVkk-',
+  iam_apikey: `${process.env.PERSONALITY_INSIGHTS_IAM_APIKEY}`,
   version: '2016-10-19',
-  url: 'https://gateway.watsonplatform.net/personality-insights/api'
+  url: `${process.env.PERSONALITY_INSIGHTS_URL}`
 });
 const db = require('../database/index.js');
 const express = require('express');
@@ -22,11 +22,11 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static(path.join(__dirname, '../src')));
-
+//get request to watson using getInsights function
 app.get('/watson', (req, res) => {
   getInsights(req.body.text, res);
 });
-
+//function to send to text to watson to retreive the value percentages that we need in order to compare the users
 function getInsights(text, res) {
   personalityInsights.profile({
     content: 'The video game industry is constantly evolving I just needed to add a couple words to make the 100 count just like technology, and I have always been an avid gamer, which is what drew me to the software field.  Being able to work with multiple new technologies on a constant basis keeps the workflow from becoming stagnant.  I have always loved building computers and being able to design and create the software makes the whole experience more complete and rewarding. I feel a great sense of accomplishment in using new tech to solve old problems, and make ideas become even more viable in today’s ever-changing tech world.',
