14bcb32f37613773fed7e8db1c29c6406cc7716d
Trevor Scott <Trvr.Scott1@gmail.com>
2019-06-20 11:56:52 -0500

    'Successfully making Watson personality call'

--- server/app.js
+++ server/app.js
@@ -31,10 +31,15 @@ function getInsights(text, res) {
   personalityInsights.profile({
     content: 'The video game industry is constantly evolving I just needed to add a couple words to make the 100 count just like technology, and I have always been an avid gamer, which is what drew me to the software field.  Being able to work with multiple new technologies on a constant basis keeps the workflow from becoming stagnant.  I have always loved building computers and being able to design and create the software makes the whole experience more complete and rewarding. I feel a great sense of accomplishment in using new tech to solve old problems, and make ideas become even more viable in today’s ever-changing tech world.',
     content_type: 'text/plain',
-    consumption_preferences: true
+    consumption_preferences: false
   })
   .then(result => {
-    console.log(JSON.stringify(result, null, 2));
+    res.send(JSON.stringify(result.values.map((value) => {
+      return {
+        name: value.name,
+        percentile: Math.trunc(value.percentile * 100)
+      }
+    }), null, 2));
   })
   .catch(err => {
     console.log('error:', err);
