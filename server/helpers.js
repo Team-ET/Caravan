9a73e715870b016de9c3ed0bba65fbcf5d3f0ec5
Trevor Scott <Trvr.Scott1@gmail.com>
2019-06-22 14:00:37 -0500

    'Added psuedo for helpers, and created groupAvg function for matching alogrithim'

--- server/helpers.js
+++ server/helpers.js
@@ -4,26 +4,39 @@ const { User, Group, User_group, Interest, Int_User } = require('../database/ind
 
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op; 
-
+// function for storing user in db
 const storeUser = (name, email, picture, pers_test, pers_percent) => User.findOrCreate({
   where: { email },
   defaults: { name, email, picture, pers_test, pers_percent } 
 });
-
+//function for storing group in db
 const storeGroup = (name, destination, date_start, date_end) => Group.findOrCreate({
   where: { name },
   defaults: { name, destination, date_start, date_end }
 });
-
+//function for getting all Groups from the db
 const findAllGroups = groups =>
  Group.findAll({
  });
-
+// function for getting all Users from the db
 const findAllUsers = (users) =>
  User.findAll(
  );
-
+// function for getting all User's Groups from the db
 const findUserGroups = userGroups => User_group.findAll({});
+
+// function for getting the avg of a group, for using when comparing the matching alogrithim
+const groupAvg = (array) => {
+  return array.reduce((total, curr) => {
+    return {
+      tradition: Math.trunc((total.tradition + curr.tradition) / person1.length), 
+      achievement: Math.trunc((total.achievement + curr.achievement) / person1.length),
+      pleasure: Math.trunc((total.pleasure + curr.pleasure) / person1.length),
+      stimulation: Math.trunc((total.stimulation + curr.stimulation) / person1.length),
+      helpfulness: Math.trunc((total.helpfulness + curr.helpfulness) / person1.length),
+    }
+  })
+}
 
 module.exports = {
   storeUser,
@@ -31,4 +44,5 @@ module.exports = {
   findAllGroups,
   findAllUsers,
   findUserGroups,
+  groupAvg
 };
