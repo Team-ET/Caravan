8b12c2b808ce285f8f380b283cabfb18f8fd8bfa
Tyl3rM <Tyl3rM@gmail.com>
2019-06-24 00:41:08 -0500

    modified:   src/app/core/components/profile/profile.component.html

--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -2,10 +2,22 @@ import { GroupsComponent } from './components/groups.component';
 import { GroupsService } from './services/groups.service'
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+  NavbarModule,
+  DropdownModule,
+  CardsFreeModule,
+  ButtonsModule,
+  IconsModule
+} from 'angular-bootstrap-md';
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule,
+    NavbarModule,
+    DropdownModule,
+    CardsFreeModule,
+    ButtonsModule,
+    IconsModule],
   declarations: [GroupsComponent],
   exports: [GroupsComponent],
   providers: [GroupsService]
