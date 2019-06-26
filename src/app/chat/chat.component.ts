177b46130ccafe4b8bf10c6c145a2e3762680dbd
EricaMcc <emccul13@gmail.com>
2019-06-26 18:10:31 -0500

    created chat componenent that gets real time messages to app

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss'],
+  providers: [ ChatService ]
+})
+export class ChatComponent implements OnInit {
+  message: string;
+  messages: string[] = [];
+
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
+
+}
