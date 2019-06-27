13ddb960f788550f2f46899f50d65fe07d5c2774
Tyl3rM <Tyl3rM@gmail.com>
2019-06-27 13:39:57 -0500

    Re-arranged "core' to just be "main"

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './main.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { GroupsModule } from '../groups/groups.module';
+import {
+  NavbarModule,
+  DropdownModule,
+  CardsFreeModule,
+  ButtonsModule,
+  IconsModule,
+  ModalModule
+} from 'angular-bootstrap-md';
+import { RouterModule } from '@angular/router';
+import { DashComponent } from './components/dash/dash.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NavbarModule,
+    IconsModule,
+    RouterModule,
+    DropdownModule.forRoot(),
+    CardsFreeModule,
+    ButtonsModule,
+    GroupsModule,
+  ],
+  declarations: [
+    ProfileComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    DashComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+  ]
+})
+export class MainModule {}
