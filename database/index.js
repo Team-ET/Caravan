06ff5c179f848e07204183f430d1840402a7a516
Tyl3rM <Tyl3rM@gmail.com>
2019-07-31 10:12:09 -0500

    Merged

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('CaravanAppDB', 'TeamET', 'Halleb0t748!', {
-  host: 'caravandbserver.database.windows.net',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USERNAME, process.env.PASSWORD, {
+  host: process.env.HOST,
   dialect: 'mssql',
   dialectOptions: {
     options: {
